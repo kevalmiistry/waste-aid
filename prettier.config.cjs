f447888eb23667694a765e563889623c7c063ed2
Keval <kevalpm@gmail.com>
2023-07-25 23:09:37 +0530

    Prettier config set

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,11 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  semi: false,
+  singleQuote: false,
+  tabWidth: 4,
+  useTabs: false,
+  trailingComma: "es5",
 };
 
 module.exports = config;
