be2bd3e2db02756f6ed2bec3a6a5c1659148f33c
Keval <kevalpm@gmail.com>
2023-07-25 23:20:17 +0530

    Formatted whole code base with updated prettier configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,28 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-  ],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: true,
+    },
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "next/core-web-vitals",
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-  },
-};
+    rules: {
+        "@typescript-eslint/consistent-type-imports": [
+            "warn",
+            {
+                prefer: "type-imports",
+                fixStyle: "inline-type-imports",
+            },
+        ],
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            { argsIgnorePattern: "^_" },
+        ],
+    },
+}
 
-module.exports = config;
+module.exports = config
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  semi: false,
-  singleQuote: false,
-  tabWidth: 4,
-  useTabs: false,
-  trailingComma: "es5",
-};
+    plugins: [require.resolve("prettier-plugin-tailwindcss")],
+    semi: false,
+    singleQuote: false,
+    tabWidth: 4,
+    useTabs: false,
+    trailingComma: "es5",
+}
 
-module.exports = config;
+module.exports = config
