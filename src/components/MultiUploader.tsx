1ebba54c2a5513a838bc3fbcd7c3397569491bd6
Keval <kevalpm@gmail.com>
2023-10-11 11:41:50 +0530

    CI: Animation improved of Image upload UI

--- src/components/MultiUploader.tsx
+++ src/components/MultiUploader.tsx
@@ -6,7 +6,7 @@ import { useUploadThing } from "~/utils/uploadthing"
 import { UploadCloud, X } from "lucide-react"
 import { useDropzone } from "@uploadthing/react/hooks"
 import { cubicBezier } from "~/utils/constants"
-import { AnimatePresence, motion } from "framer-motion"
+import { motion } from "framer-motion"
 
 export const MultiUploader = () => {
     const [files, setFiles] = useState<File[]>([])
@@ -64,46 +64,43 @@ export const MultiUploader = () => {
                 </div>
             </div>
 
-            {files.length > 0 && (
-                <>
-                    <div
-                        className={
-                            "custom-scrollbar flex gap-2 overflow-x-auto py-2"
-                        }
-                    >
-                        <AnimatePresence presenceAffectsLayout={false}>
-                            {files.map((file, idx) => (
-                                <motion.div
-                                    initial={{ opacity: 0, scale: 0.8 }}
-                                    animate={{ opacity: 1, scale: 1 }}
-                                    exit={{ opacity: 0, scale: 0.8 }}
-                                    key={idx}
-                                    className={`group sticky left-0 h-[120px] min-w-[120px] max-w-[120px] overflow-hidden`}
-                                >
-                                    <img
-                                        src={URL.createObjectURL(file)}
-                                        alt="img"
-                                        className={`h-full rounded-md object-cover transition-all group-hover:scale-110`}
-                                    />
-                                    <X
-                                        className="absolute left-1/2 top-1/2 z-[2] -translate-x-1/2 -translate-y-1/2 scale-0 cursor-pointer rounded-full bg-red-500 p-1 text-white transition-all group-hover:scale-100"
-                                        onClick={() => handleImageRemove(idx)}
-                                    />
-                                    <div className="absolute inset-0 z-[1] h-full w-full rounded-md bg-[#00000070] opacity-0 transition-all group-hover:opacity-100"></div>
-                                </motion.div>
-                            ))}
-                        </AnimatePresence>
-                    </div>
-                    <div className="my-5 flex justify-center">
-                        <button
-                            className="btn-secondary text-center"
-                            onClick={() => startUpload(files)}
-                        >
-                            Upload {files.length} files
-                        </button>
-                    </div>
-                </>
-            )}
+            <div className={"custom-scrollbar flex overflow-x-auto py-2"}>
+                {files.length > 0
+                    ? files.map((file, idx) => (
+                          <motion.div
+                              initial={{ opacity: 0, scale: 0.8 }}
+                              animate={{ opacity: 1, scale: 1 }}
+                              transition={{
+                                  delay: (idx * 0.5) / 10,
+                                  ease: cubicBezier,
+                              }}
+                              key={idx}
+                              className={`group sticky left-0 h-[120px] min-w-[120px] max-w-[120px] overflow-hidden`}
+                              style={{ translateX: `${idx * 10}px` }}
+                          >
+                              <img
+                                  src={URL.createObjectURL(file)}
+                                  alt="img"
+                                  className={`h-full rounded-md object-cover transition-all group-hover:scale-110`}
+                              />
+                              <X
+                                  className="absolute left-1/2 top-1/2 z-[2] -translate-x-1/2 -translate-y-1/2 scale-0 cursor-pointer rounded-full bg-red-500 p-1 text-white transition-all group-hover:scale-100"
+                                  onClick={() => handleImageRemove(idx)}
+                              />
+                              <div className="absolute inset-0 z-[1] h-full w-full rounded-md bg-[#00000070] opacity-0 transition-all group-hover:opacity-100"></div>
+                          </motion.div>
+                      ))
+                    : null}
+            </div>
+
+            {/* <div className="my-5 flex justify-center">
+                <button
+                    className="btn-secondary text-center"
+                    onClick={() => startUpload(files)}
+                >
+                    Upload {files.length} files
+                </button>
+            </div> */}
         </>
     )
 }
