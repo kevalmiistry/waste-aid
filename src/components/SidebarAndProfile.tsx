1b3a54fff9becf57b0856f0d24d55ffde25bfdf2
Keval <kevalpm@gmail.com>
2023-11-24 22:48:29 +0530

    FIX: SidebarAndProfile overflow issue in mobile-view fix

--- src/components/SidebarAndProfile.tsx
+++ src/components/SidebarAndProfile.tsx
@@ -12,10 +12,12 @@ const SidebarAndProfile: FC<ISidebarAndProfile> = ({ children }) => {
     const [openProfile, setOpenProfile] = useState(false)
     const [openMenu, setOpenMenu] = useState(false)
 
+    const closeMenu = () => setOpenMenu(false)
+
     return (
         <div className="h-screen overflow-hidden">
             <Notification />
-            <div className="flex h-screen">
+            <div className="relative flex h-screen overflow-hidden">
                 <div
                     aria-label="sidebar"
                     className={`absolute left-0 right-0 z-[2] border-t bg-white p-5 text-xl font-medium shadow-[3px_-15px_82px_-25px_rgba(0,0,0,0.5)] transition-all md:static md:z-[0] md:flex-[1.5] md:p-4 md:shadow-none ${
@@ -25,18 +27,18 @@ const SidebarAndProfile: FC<ISidebarAndProfile> = ({ children }) => {
                     <div className="flex justify-end">
                         <X
                             size={"1.75rem"}
-                            onClick={() => setOpenMenu(false)}
+                            onClick={closeMenu}
                             className="md:hidden"
                         />
                     </div>
                     <ul className="flex flex-col items-center gap-4">
-                        <Link href={"/"}>
+                        <Link href={"/"} onClick={closeMenu}>
                             <li>Home</li>
                         </Link>
-                        <Link href={"/aid-man"}>
+                        <Link href={"/aid-man"} onClick={closeMenu}>
                             <li>Aid-Man</li>
                         </Link>
-                        <Link href={"/welcome"}>
+                        <Link href={"/welcome"} onClick={closeMenu}>
                             <li>Welcome</li>
                         </Link>
                     </ul>
@@ -49,7 +51,7 @@ const SidebarAndProfile: FC<ISidebarAndProfile> = ({ children }) => {
                         {children}
                         <div
                             aria-label="mobile-view-bottom-bar"
-                            className="absolute bottom-0 left-0 right-0 flex justify-around border p-3 md:hidden"
+                            className="fixed bottom-0 left-0 right-0 flex justify-around border bg-white p-3 md:hidden"
                         >
                             <button onClick={() => setOpenMenu(true)}>
                                 <Menu size={"1.75rem"} />
