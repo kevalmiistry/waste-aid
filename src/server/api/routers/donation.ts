cfc847b7e66bb23dcef5e529b716ecdad5e1afd4
Keval <kevalpm@gmail.com>
2023-12-04 23:12:36 +0530

    FEAT: add 'verifyDonation' endpoint to verify donation token

--- src/server/api/routers/donation.ts
+++ src/server/api/routers/donation.ts
@@ -1,4 +1,5 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc"
+import { TRPCError } from "@trpc/server"
 import { z } from "zod"
 
 export const donationRouter = createTRPCRouter({
@@ -16,6 +17,61 @@ export const donationRouter = createTRPCRouter({
                     donator_id: ctx.session.user.id,
                 },
             })
+        }),
+
+    verifyDonation: protectedProcedure
+        .input(
+            z.object({
+                donation_id: z.string(),
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            const donationWithPost = await ctx.prisma.donations.findUnique({
+                where: {
+                    uuid: input.donation_id,
+                },
+                include: {
+                    post: true,
+                },
+            })
+
+            if (donationWithPost?.post.am_id !== ctx.session.user.id) {
+                throw new TRPCError({
+                    code: "UNAUTHORIZED",
+                    message:
+                        "You're not the Aid-man of the Post this QR code is generated for!",
+                })
+            }
+            if (donationWithPost?.reached === true) {
+                throw new TRPCError({
+                    code: "FORBIDDEN",
+                    message: "This token is already verified!",
+                })
+            }
+
+            await ctx.prisma.donations.update({
+                data: {
+                    reached: true,
+                    reachedDate: new Date(),
+                },
+                where: {
+                    uuid: donationWithPost.uuid,
+                },
+            })
+
+            const oldAmount = Number(donationWithPost.post.collectedAmount)
+            const newAmount = oldAmount + Number(donationWithPost.donatedAmout)
+
+            await ctx.prisma.post.update({
+                data: {
+                    collectedAmount: newAmount,
+                },
+                where: {
+                    uuid: donationWithPost.post.uuid,
+                },
+            })
+
+            return { oldAmount, newAmount }
         }),
 
     listDonations: protectedProcedure.query(async ({ ctx }) => {
